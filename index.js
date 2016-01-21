67615a4f10d103368e76feaca6f178eef5cada61
Xotic750 <xotic750@gmail.com>
2016-01-21 16:48:56 +0100

    Update dependencies, version

--- index.js
+++ index.js
@@ -39,7 +39,7 @@
  * `es6.shim.js` provides compatibility shims so that legacy JavaScript engines
  * behave as closely as possible to ECMAScript 6 (Harmony).
  *
- * @version 1.0.5
+ * @version 1.0.6
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
@@ -52,21 +52,29 @@
   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
   es3:true, esnext:false, plusplus:true, maxparams:1, maxdepth:1,
-  maxstatements:8, maxcomplexity:4 */
+  maxstatements:12, maxcomplexity:4 */
 
 /*global module */
 
 ;(function () {
   'use strict';
 
-  var pPush = Array.prototype.push,
-    pJoin = Array.prototype.join,
-    ES = require('es-abstract/es6'),
-    defProps = require('define-properties'),
-    defProp = require('define-property-x'),
-    $max = Math.max,
-    $floor = Math.floor,
-    BigC;
+  var pPush = Array.prototype.push;
+  var pJoin = Array.prototype.join;
+  var define = require('define-properties-x');
+  var $max = Math.max;
+  var $floor = Math.floor;
+
+  /**
+   * Predicate that always returns `true` (constant).
+   *
+   * @private
+   * @return {boolean} Returns `true`.
+   */
+  function truePredicate() {
+    return true;
+  }
+
   /**
    * Increments the counter's value by `1`.
    *
@@ -75,21 +83,21 @@
    */
   function counterNext() {
     /*jshint validthis:true */
-    var result = [],
-      length = this.count.length,
-      howMany = $max(length, 1),
-      carry = 0,
-      index = 0,
-      zi;
+    var result = [];
+    var length = this.count.length;
+    var howMany = $max(length, 1);
+    var carry = 0;
+    var index = 0;
     while (index < howMany || carry) {
-      zi = carry + (index < length ? this.count[index] : 0) + !index;
-      ES.Call(pPush, result, [zi % 10]);
+      var zi = carry + (index < length ? this.count[index] : 0) + !index;
+      pPush.call(result, zi % 10);
       carry = $floor(zi / 10);
       index += 1;
     }
     this.count = result;
     return this;
   }
+
   /**
    * Serialise the counter's current value.
    *
@@ -99,52 +107,23 @@
    */
   function counterToString() {
     /*jshint validthis:true */
-    return ES.Call(pJoin, this.count, ['']);
+    return pJoin.call(this.count, '');
   }
+
   /**
-   * Incremental integer counter. Counts from `0` to very big intergers.
-   * Javascript's number type allows you to count in integer steps
-   * from `0` to `9007199254740991`. As of ES5, Strings can contain
-   * approximately 65000 characters and ES6 is supposed to handle
-   * the `MAX_SAFE_INTEGER` (though I don't believe any environments supports
-   * this). This counter represents integer values as strings and can therefore
-   * count in integer steps from `0` to the maximum string length (that's some
-   * 65000 digits). In the lower range, upto `9007199254740991`, the strings can
-   * be converted to safe Javascript integers `Number(value)` or `+value`. This
-   * counter is great for any applications that need a really big count
-   * represented as a string, (an ID string).
+   * The BigCounter class.
    *
+   * @private
    * @constructor
-   * @example
-   * var BigCounter = require('big-counter-x');
-   * var counter = new BigCounter();
-   *
-   * counter.get(); // '0'
-   * counter.next(); // counter object
-   * counter.get(); // '1'
-   *
-   * // Methods are chainable.
-   * counter.inc().next(); // counter object
-   * counter.get(); // '3'
-   *
-   * counter.reset(); // counter object
-   * counter.get(); // '0'
-   * counter.toString(); // '0'
-   * counter.valueOf(); // '0'
-   * counter.toJSON(); // '0'
-   *
-   * // Values upto `9007199254740991` convert to numbers.
-   * Number(counter); // 0
-   * +counter; // 0
    */
-  module.exports = BigC = function BigCounter() {
+  function BigCounter() {
     /* istanbul ignore if */
-    if (!(this instanceof BigC)) {
-      return new BigC();
+    if (!this || !(this instanceof BigCounter)) {
+      return new BigCounter();
     }
-    defProp(this, 'count', [0]);
-  };
-  defProps(BigC.prototype, {
+    define.defineProperty(this, 'count', [0]);
+  }
+  define.defineProperties(BigCounter.prototype, {
     /**
      * Increments the counter's value by `1`.
      *
@@ -195,12 +174,50 @@
      */
     reset: function () {
       this.count.length = 0;
-      ES.Call(pPush, this.count, [0]);
+      pPush.call(this.count, 0);
       return this;
     }
   }, {
-    valueOf: function () { return true; },
-    toString: function () { return true; },
-    toJSON: function () { return true; }
+    valueOf: truePredicate,
+    toString: truePredicate,
+    toJSON: truePredicate
   });
+
+  /**
+   * Incremental integer counter. Counts from `0` to very big intergers.
+   * Javascript's number type allows you to count in integer steps
+   * from `0` to `9007199254740991`. As of ES5, Strings can contain
+   * approximately 65000 characters and ES6 is supposed to handle
+   * the `MAX_SAFE_INTEGER` (though I don't believe any environments supports
+   * this). This counter represents integer values as strings and can therefore
+   * count in integer steps from `0` to the maximum string length (that's some
+   * 65000 digits). In the lower range, upto `9007199254740991`, the strings can
+   * be converted to safe Javascript integers `Number(value)` or `+value`. This
+   * counter is great for any applications that need a really big count
+   * represented as a string, (an ID string).
+   *
+   * @constructor
+   * @example
+   * var BigCounter = require('big-counter-x');
+   * var counter = new BigCounter();
+   *
+   * counter.get(); // '0'
+   * counter.next(); // counter object
+   * counter.get(); // '1'
+   *
+   * // Methods are chainable.
+   * counter.inc().next(); // counter object
+   * counter.get(); // '3'
+   *
+   * counter.reset(); // counter object
+   * counter.get(); // '0'
+   * counter.toString(); // '0'
+   * counter.valueOf(); // '0'
+   * counter.toJSON(); // '0'
+   *
+   * // Values upto `9007199254740991` convert to numbers.
+   * Number(counter); // 0
+   * +counter; // 0
+   */
+  module.exports = BigCounter;
 }());
